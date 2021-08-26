75b10171addb9112a0a2871f8bf127061cc8084d
Marcos V. Piesske <mv.piesske@gmail.com>
2021-08-25 23:20:33 -0300

    Add go.mod and go.sum to project and cmd with main function

---
+++ cmd/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"github.com/google/uuid"
+	"github.com/marcos-dev88/go-password-generator/application"
+	"github.com/marcos-dev88/go-password-generator/domain/entity"
+	service2 "github.com/marcos-dev88/go-password-generator/domain/service"
+	"github.com/marcos-dev88/go-password-generator/infrastructure/persistence"
+	"github.com/marcos-dev88/go-password-generator/infrastructure/ui"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func init() {
+	if err := defineEnvs(".env"); err != nil {
+		log.Printf("Error to load .env -> %v", err)
+	}
+}
+
+func main() {
+	db := persistence.NewMongoDB("", "", "")
+	repo := persistence.NewRepository(db)
+
+	newUuid, err := uuid.NewUUID()
+
+	if err != nil {
+		panic(err)
+	}
+
+	passwordGen := entity.NewPasswordGen(
+		newUuid,
+		"",
+		0,
+		false,
+		false,
+		false,
+	)
+
+	service := service2.NewService(passwordGen)
+
+	app := application.NewApplication(repo, service)
+
+	handler := ui.NewHandler(app)
+
+	log.Printf("Server is running at: %s", ":8000")
+	http.HandleFunc("/password-gen/", handler.HandlePasswordGenerator)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+
+}
+
+func defineEnvs(fileName string) error {
+
+	envs := make(map[string]string)
+
+	file, err := os.Open(fileName)
+
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatalf("We got an error -> %v", err)
+		}
+	}(file)
+
+	if err != nil {
+		return err
+	}
+
+	sc := bufio.NewScanner(file)
+
+	for sc.Scan() {
+		envSplit := strings.SplitN(sc.Text(), "=", 2)
+		if len(envSplit) > 1 {
+			envs[envSplit[0]] = envSplit[1]
+		}
+	}
+
+	for key, value := range envs {
+		if err := os.Setenv(key, value); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
