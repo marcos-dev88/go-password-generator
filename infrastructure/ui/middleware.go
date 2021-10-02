f3148a3e3ea0801214338211ba0040fe6152ab9f
Marcos Vitor <mv.piesske@gmail.com>
2021-10-02 00:35:12 -0300

    feature-16 - implementing new method to enable cors

--- infrastructure/ui/middleware.go
+++ infrastructure/ui/middleware.go
@@ -2,31 +2,54 @@ package ui
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"os"
 )
 
-type Middleware interface{
+type Middleware interface {
 	Auth(handler http.HandlerFunc) http.HandlerFunc
+	EnablingCORS(handler http.HandlerFunc) http.HandlerFunc
 }
 
-type middleware struct {}
+type middleware struct{}
 
-func NewMiddleware() *middleware{
+func NewMiddleware() *middleware {
 	return &middleware{}
 }
 
 func (m *middleware) Auth(handler http.HandlerFunc) http.HandlerFunc {
-	return func (rw http.ResponseWriter, req *http.Request) {
+	return func(rw http.ResponseWriter, req *http.Request) {
+		rw.Header().Set("Content-Type", "application-json")
+
 		apiKey := req.Header.Get("api-token")
 		if apiKey != os.Getenv("API_KEY") {
-
-			returnMessage, _ := json.Marshal(map[string]string{"error": "api-key is missing or isn't correct"})
-			rw.Header().Set("Content-Type", "application-json")
+			returnMessage, _ := json.Marshal(map[string]string{"error": "api-key is missing or it isn't correct"})
 			rw.WriteHeader(http.StatusBadRequest)
 			rw.Write(returnMessage)
 			return
 		}
+		handler.ServeHTTP(rw, req)
+	}
+}
+
+func (m *middleware) EnablingCORS(handler http.HandlerFunc) http.HandlerFunc {
+	return func(rw http.ResponseWriter, req *http.Request) {
+
+		rw.Header().Set("Access-Control-Allow-Origin", "*")
+		rw.Header().Set("Access-Control-Allow-Credentials", "true")
+		rw.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		rw.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
+
+		if req.Method != "POST" {
+			log.Printf("method not allowed")
+			returnMessage, _ := json.Marshal(map[string]string{"error": "method not allowed"})
+			rw.WriteHeader(http.StatusMethodNotAllowed)
+			rw.Write(returnMessage)
+			return
+		}
+
+		rw.WriteHeader(http.StatusOK)
 		handler.ServeHTTP(rw, req)
 	}
 }
