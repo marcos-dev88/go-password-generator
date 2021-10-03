b8d96b5073284b5737a60f37a7065d669e622e05
Marcos Vitor <mv.piesske@gmail.com>
2021-10-03 19:19:30 -0300

    Creating an way to response error if it's present

--- infrastructure/http_response/response_error.go
+++ infrastructure/http_response/response_error.go
@@ -1,1 +1,32 @@
 package http_response
+
+import "log"
+
+type CustomError interface {
+	DefaultLogResponse()
+}
+
+type customError struct {
+	StatusCode int
+	Message    string
+}
+
+func NewCustomError(statusCode int, message string) *customError {
+	return &customError{StatusCode: statusCode, Message: message}
+}
+
+func (ce *customError) Error() string {
+	return ce.Message
+}
+
+func GetErrorMessage(err error) string {
+	return err.(*customError).Message
+}
+
+func GetErrorStatus(err error) int {
+	return err.(*customError).StatusCode
+}
+
+func (ce *customError) DefaultLogResponse() {
+	log.Printf("\nstatus: %v \nmessage: %v", ce.StatusCode, ce.Message)
+}
