0a26fb47296bb7a80ae29e7288e477bf70d9b4ae
Marcos Vitor <mv.piesske@gmail.com>
2021-10-03 19:18:40 -0300

    Creating new package to define http response

---
+++ infrastructure/http_response/response_json.go
@@ -0,0 +1,41 @@
+package http_response
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type ResponseHTTP interface {
+	ResponseJSON(rw http.ResponseWriter, resp *responseHttp)
+	ErrorJSON(rw http.ResponseWriter, ce customError)
+}
+
+type responseHttp struct {
+	StatusCode int
+	Payload interface{}
+}
+
+func NewResponseHTTP(statusCode int, payload interface{}) *responseHttp {
+	return &responseHttp{StatusCode: statusCode, Payload: payload}
+}
+
+func (rh *responseHttp) ResponseJSON(rw http.ResponseWriter, resp *responseHttp){
+
+	response, err := json.Marshal(&resp.Payload)
+
+	if err != nil {
+		rh.ErrorJSON(rw, *NewCustomError(GetErrorStatus(err), GetErrorMessage(err)))
+	}
+
+	rw.Header().Set("Content-Type", "application-json")
+	rw.WriteHeader(resp.StatusCode)
+	rw.Write(response)
+}
+
+func (rh *responseHttp) ErrorJSON(rw http.ResponseWriter, ce customError) {
+	ce.DefaultLogResponse()
+	rh.ResponseJSON(rw, &responseHttp{
+		StatusCode: ce.StatusCode ,
+		Payload:    map[string]interface{}{"error": ce.Message, "status": ce.StatusCode},
+	})
+}
