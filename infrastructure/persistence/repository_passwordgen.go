751dc3ba9c00988bcc566811a8d1952d273c269f
Marcos Vitor <mv.piesske@gmail.com>
2021-12-02 21:59:44 -0300

    Removing unused code

--- infrastructure/persistence/repository_passwordgen.go
+++ infrastructure/persistence/repository_passwordgen.go
@@ -9,12 +9,6 @@ import (
 	"log"
 )
 
-type Repository interface {
-	GetLastTenPasswords() ([]*entity.PasswordGen, error)
-	SavePasswordGen(*entity.PasswordGen) (*entity.PasswordGen, error)
-	PasswordExists(password string) (bool, error)
-}
-
 type repository struct {
 	mongodb MongoDB
 }
