35d02e749738d4f46f8a465fdb7fdb5f00a7b192
Marcos <mv.piesske@gmail.com>
2022-02-13 00:44:17 -0300

    Improving docker network create network script

--- scripts/docker/network_config.sh
+++ scripts/docker/network_config.sh
@@ -2,8 +2,8 @@
 
 OUTADDRSHOW=$(ip addr show | grep 206.8.3.1/24)
 
-if [[ OUTADDRSHOW != "" ]]; then
-    docker network create --subnet 206.8.3.0/24 password_gen_net_dev
-else
+if [ ! -z "$OUTADDRSHOW" -a "$OUTADDRSHOW" != " " ]; then
     echo "network already created, initializing..."
+else
+    docker network create --subnet 206.8.3.0/24 password_gen_net_dev
 fi
